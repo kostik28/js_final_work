f948977869724513fe7037af7e9a0273b17e3412
Nikitin Konstantin <kostik28@list.ru>
2019-02-25 22:58:28 +0300

    Style start-2

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -17,29 +17,55 @@ class Posts extends React.Component{
     this.props.userActions.onLoadUsers();
   }
 
+  makeCounter() {
+    let currentCount = 0;
+    return () => currentCount++
+  };
+
+  displayCol(arr, rest, counter) {
+    return arr.filter((item, i) => i % 3 === rest)
+    .map(post => {
+      return (
+        <div className='posts-blok' key={counter()}>
+          <article className='posts-article'>
+
+          </article>
+          <p>{post.id}, email: {post.title}</p>
+          <Link
+            to={'/posts/' + post.id}
+          >
+            перейти к посту
+          </Link>
+        </div>
+      )
+    })
+  };
+
+  makeCounter() {
+    let currentCount = 1;
+    return () => currentCount++
+  };
+
   render() {
+    const counter = this.makeCounter();
     return(
-      <div>
+      <div className='posts'>
+
         <h4>Список постов</h4>
+
         {this.props.loggedUser !== null &&
           <button
             onClick={(e) => this.props.modalActions.onOpenModal(e.target.innerHTML)}>
             new post
-          </button>}
-        {
-          this.props.posts.map((post, i) => {
-            return (
-              <div key={i}>
-                <p>{post.id}, email: {post.title}</p>
-                <Link
-                  to={'/posts/' + post.id}
-                >
-                  перейти к посту
-                </Link>
-              </div>
-            );
-          })
+          </button>
         }
+
+        <div className='posts-inner'>
+          <div className='posts-container'>{this.displayCol(this.props.posts, 0, counter)}</div>
+          <div className='posts-container'>{this.displayCol(this.props.posts, 1, counter)}</div>
+          <div className='posts-container'>{this.displayCol(this.props.posts, 2, counter)}</div>
+        </div>
+
       </div>
     )
   }
