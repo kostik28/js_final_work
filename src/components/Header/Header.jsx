31aa50ce81fbe64bb8fd1e5a68884ee6eb1c782b
Nikitin Konstantin <kostik28@list.ru>
2019-03-10 21:19:39 +0300

    Finish

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,14 +3,6 @@ import { Link } from 'react-router-dom';
 
 export default (props) => {
 
-  function getLogin(text) {
-    let sliced = text.slice(0, 9);
-    if (sliced.length < text.length) {
-      sliced += '...';
-    }
-    return sliced;
-    }
-
   function completeMenu() {
     return (
       props.pages.map((obj, i) => {
@@ -54,7 +46,7 @@ export default (props) => {
 
     const loggedUser = props.loggedUser;
     if(loggedUser !== null && obj.name === 'login') {
-      linkName = 'Hi, ' + getLogin(loggedUser.login)
+      linkName = 'Hi, ' + loggedUser.login
     }
 
     let block;
